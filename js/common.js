78e481bbc23bb6bbac142dbd2ccb71ce96966530
rayan <mrayan8@gmail.com>
2019-01-31 13:32:28 +0500

    add

--- js/common.js
+++ js/common.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
+	
 	$(".mc_toggle").click(function(){
+		
 		$(this).parent().parent().children("ul").slideToggle();
+		
 	});
 
 	//Цели для Яндекс.Метрики и Google Analytics
